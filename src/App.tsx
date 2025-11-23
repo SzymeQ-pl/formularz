cf3430c3d1e77f0fe9c7dcf134412460fda26706
Szymon <szymeq192@gmail.com>
2025-11-23 14:20:37 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type FormDataType = {
+  imie: string;
+  nazwisko: string;
+  ulica: string;
+  miasto: string;
+};
+
+export default function App() {
+  const [data, setData] = useState<FormDataType | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const formObject = Object.fromEntries(form) as FormDataType;
+    setData(formObject);
+  };
+
+  if (data) {
+    return (
+      <table border={1} style={{ margin: 20 }}>
+        <tbody>
+          <tr>
+            <td>Imię</td>
+            <td>{data.imie}</td>
+          </tr>
+          <tr>
+            <td>Nazwisko</td>
+            <td>{data.nazwisko}</td>
+          </tr>
+          <tr>
+            <td>Ulica</td>
+            <td>{data.ulica}</td>
+          </tr>
+          <tr>
+            <td>Miasto</td>
+            <td>{data.miasto}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        width: 200,
+        margin: 20,
+      }}
+    >
+      <input name="imie" placeholder="Imię" />
+      <input name="nazwisko" placeholder="Nazwisko" />
+      <input name="ulica" placeholder="Ulica" />
+      <input name="miasto" placeholder="Miasto" />
+      <button type="submit">Prześlij</button>
+    </form>
+  );
 }
-
-export default App
